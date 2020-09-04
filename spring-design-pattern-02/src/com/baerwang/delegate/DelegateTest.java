7b2437f7ca429e93eab29a50818ad5b0b76f3336
baerwang <baerwang@126.com>
2020-09-04 17:44:35 +0800

    commit

---
+++ spring-design-pattern-02/src/com/baerwang/delegate/DelegateTest.java
@@ -0,0 +1,15 @@
+package com.baerwang.delegate;
+
+/**
+ * 委派模式（Delegate Pattern）
+ * <p>
+ * 委派模式不属于GOF23种设计模式。委派模式的基本作用就是负责任务的调用和分配，跟代理模式很像，
+ * 可以看作一个特殊情况下的静态全权代理，但是代理模式注重过程，而委派模式注重结果。
+ *
+ * </p>
+ *
+ * @author baerwang
+ * @since 2020/9/4 11:18
+ */
+public class DelegateTest {
+}
